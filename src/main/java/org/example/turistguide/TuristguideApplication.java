e2350b1d3bb74d2c13b2d2128625f379ae13dcbc
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-08-27 14:13:03 +0200

    1

--- src/main/java/org/example/turistguide/TuristguideApplication.java
+++ src/main/java/org/example/turistguide/TuristguideApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TuristguideApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TuristguideApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TuristguideApplication.class, args);
+    }
 
 }
