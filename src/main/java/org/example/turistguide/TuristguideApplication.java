1aebbdcf6397325a95211f811f2d93f3b9121afb
Kwanjai <65347403+Kwan1992@users.noreply.github.com>
2026-08-27 14:08:18 +0200

    Initial commit

---
+++ src/main/java/org/example/turistguide/TuristguideApplication.java
@@ -0,0 +1,13 @@
+package org.example.turistguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/turistguide/TuristguideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turistguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
