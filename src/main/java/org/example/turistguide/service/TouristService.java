05e1b44d1c2cb3e779ac354d114a027b369661ad
celineewu <celineewu@gmail.com>
2026-09-01 13:37:57 +0200

    update Service

--- src/main/java/org/example/turistguide/service/TouristService.java
+++ src/main/java/org/example/turistguide/service/TouristService.java
@@ -1,4 +1,39 @@
 package org.example.turistguide.service;
 
+import org.example.turistguide.model.TouristAttraction;
+import org.example.turistguide.repository.TouristRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
 public class TouristService {
+    private final TouristRepository touristRepository;
+
+    public TouristService(TouristRepository touristRepository) {
+        this.touristRepository = touristRepository;
+    }
+
+    public List<TouristAttraction> getAllTouristAttractions() {
+        return touristRepository.getTouristAttractions();
+    }
+
+    public TouristAttraction getTouristAttractionByName(String name) {
+        TouristAttraction attraction = touristRepository.findTouristAttractionByName(name);
+        if (attraction == null) {
+            throw new IllegalArgumentException("Tourist attraction not found: " + name);
+        }
+        return attraction;
+    }
+
+    public List<TouristAttraction> getTouristAttractionsByDescription(String keyword) {
+        return touristRepository.findTouristAttractionsByDescription(keyword);
+    }
 }
+
+
+
+
+
+
